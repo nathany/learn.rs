c3c8d950f321dfacae5d6d0c623180f70d988a09
Nathan Youngman <git@nathany.com>
2025-07-12 18:40:36 -0600

    simplify to just c_long

--- hello_ffi/src/main.rs
+++ hello_ffi/src/main.rs
@@ -1,16 +1,12 @@
-use std::ffi::{c_int, c_long};
+use std::ffi::c_long;
 
 unsafe extern "C" {
-    safe fn abs(input: c_int) -> c_int;
     safe fn labs(input: c_long) -> c_long;
 }
 
 fn main() {
-    let num1: i32 = -42;
-    println!("Absolute value of -3 according to C: {}", abs(num1));
-
-    let num2: i64 = -9876543210;
-    println!("Absolute value of {num2} according to C: {}", labs(num2));
+    let num: i64 = -9876543210;
+    println!("Absolute value of {num} according to C: {}", labs(num));
     // On Windows this is error[E0308]: mismatched types, but on macOS it's not considered an error.
 }
 
